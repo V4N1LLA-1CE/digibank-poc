2978d12fc7c81b369e205c7966c489e812569cd1
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-10-21 23:45:02 +1100

    feat: add repositories

---
+++ accounts/src/main/java/com/digibank/accounts/repository/AccountsRepository.java
@@ -0,0 +1,8 @@
+package com.digibank.accounts.repository;
+
+import com.digibank.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {}
---
+++ accounts/src/main/java/com/digibank/accounts/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.digibank.accounts.repository;
+
+import com.digibank.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
