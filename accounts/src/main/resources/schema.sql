99b5d2c5d659567be9d50093df528870af6e359b
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-10-21 23:54:27 +1100

    chore(db): change field name

--- accounts/src/main/resources/schema.sql
+++ accounts/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS customer (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     mobile_number VARCHAR(20) NOT NULL,
     created_at DATE NOT NULL,
